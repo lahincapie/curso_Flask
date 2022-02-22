f9f794eb4bc169974fa1c8706b57511c05400b20
lahincapie <alejandra612@hotmail.com>
2022-02-22 17:42:01 -0500

    update

--- platzi_flask/main.py
+++ platzi_flask/main.py
@@ -1,32 +1,24 @@
-'''1) importancion de la clase Flask '''
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect, render_template
 
-'''2) crear una instancia de Flask:
-    se crea una variable app y se
-    llama a la clasde Flask que por
-    parametro tiene el nombre de la
-    aplicacion que en este caso es
-    el nombre del archivo (main.py)
-    indicado por __name__
-'''
+
 app = Flask(__name__)
 
-'''4) con el decorador @app.route()
-que recibe como parametro la ruta en
-donde se va correr esta funcion'''
 @app.route('/')
-def hello():
-    '''3) se crea la funcion que esn este
-    caso mostrara un saludo'''
-    '''para conocer la direccion ip del
-    usuario se puede usar request.remote_addr'''
+def index():
     user_ip = request.remote_addr
-    return f'Hello World, tu IP es{user_ip}'
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
 
 
-'''Otra forma poner el modo debuggin en flask
-es enviando por prametro debug = True, tambien
-se puede enviar otros parametros'''
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    return f'Hello World , tu IP es: {user_ip}'
+
+
+
 if __name__ == '__main__':
     app.run(port = 5000, debug = True)
 
