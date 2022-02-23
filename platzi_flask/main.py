0f88889a88c5ff95842719077fbb784e7c1f1333
lahincapie <alejandra612@hotmail.com>
2022-02-22 22:25:23 -0500

    Configurar páginas de error

--- platzi_flask/main.py
+++ platzi_flask/main.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 
 todos = ['Blue', 'Red', 'Green', 'Black',]
 
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
